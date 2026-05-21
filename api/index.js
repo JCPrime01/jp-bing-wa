ff36b6cc184f1281928dea2821913532864af71c
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-21 10:21:30 -0300

    chore: update shareCode to QGD2Z5981GU

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=ATN7IPE84XZ&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=QGD2Z5981GU&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
