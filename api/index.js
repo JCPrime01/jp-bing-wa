01c9d405f7a40cd5768bfacb242fc434f38dc6d1
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-24 09:46:39 -0300

    chore: update shareCode to 32YGBAJCV56

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=XJIRW0IDDUY&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=32YGBAJCV56&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
