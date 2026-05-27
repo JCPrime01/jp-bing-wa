c728af8baaa322e6978675220a099e8741a103da
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-27 17:01:16 -0300

    chore: update shareCode to N3L1HO3OGOR

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=1D1VIFEQE7A&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=N3L1HO3OGOR&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
