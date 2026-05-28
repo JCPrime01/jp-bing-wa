ec93d543cd73ec3dffb38402a34347084df9572b
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-27 21:48:48 -0300

    chore: update shareCode to 11GPRQMEI86

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=ATW41NTYJ8M&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=11GPRQMEI86&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
