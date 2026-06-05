d47151245183d75ac01fb85305efbac5ed1bfe39
JCPrime01 <vanzokiller@gmail.com>
2026-06-05 19:34:23 -0300

    chore: shareCode H8RMSAMUZHS -> IWK4KCRHAVT (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=H8RMSAMUZHS&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=IWK4KCRHAVT&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
