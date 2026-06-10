e387e5072c4652e2141f01312cab1a99f59720fb
JCPrime01 <vanzokiller@gmail.com>
2026-06-10 09:39:10 -0300

    chore: shareCode 2A08RU0LIZL -> JPIP7JACNKW (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=2A08RU0LIZL&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=JPIP7JACNKW&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
