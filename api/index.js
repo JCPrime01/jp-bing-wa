4883c08fc00a113f36c97b797d91134cb59d7738
JCPrime01 <vanzokiller@gmail.com>
2026-06-11 20:32:18 -0300

    chore: shareCode T23A6BYK2KS -> 3SMUC32X7E3 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=T23A6BYK2KS&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=3SMUC32X7E3&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
