7136b3c147978439e144946295e6fb0b58bb60a1
JCPrime01 <vanzokiller@gmail.com>
2026-06-13 10:01:33 -0300

    chore: shareCode 6YN95KWOYQ5 -> WMD3D5NVNOX (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=6YN95KWOYQ5&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=WMD3D5NVNOX&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
