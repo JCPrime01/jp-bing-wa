17adda15d6723e64087ccbc64735741d61c860af
JCPrime01 <vanzokiller@gmail.com>
2026-06-19 09:04:33 -0300

    chore: shareCode XRJE4LYSDLP -> Z3URF9S3IM4 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=XRJE4LYSDLP&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=Z3URF9S3IM4&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
