9d3ebf724c99430588f4e27049c18074511f6dab
JCPrime01 <vanzokiller@gmail.com>
2026-06-25 10:20:15 -0300

    chore: shareCode 5TW9KRSCZZO -> EVW4RJIXZFJ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=5TW9KRSCZZO&afp1=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=EVW4RJIXZFJ&afp1=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
