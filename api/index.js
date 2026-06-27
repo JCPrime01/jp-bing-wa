26c441b0724b50d142c2680944281151769631de
JCPrime01 <vanzokiller@gmail.com>
2026-06-26 23:58:57 -0300

    chore: shareCode ZM00870RPB8 -> AEYL214YTVV (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=ZM00870RPB8&afp1=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=AEYL214YTVV&afp1=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
