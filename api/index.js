097d56d46b1af0df2deb5c9378e5a513437840ce
JCPrime01 <vanzokiller@gmail.com>
2026-06-28 01:50:33 -0300

    chore: shareCode AEYL214YTVV -> EGDFUGHR1MT (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=AEYL214YTVV&afp1=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=EGDFUGHR1MT&afp1=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
