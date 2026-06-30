37bdff3d31cb57a3e6dbf312b7b4f056f9da0426
JCPrime01 <vanzokiller@gmail.com>
2026-06-30 09:21:05 -0300

    chore: shareCode OFU1VE0JODS -> WTQYJFRG5YI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=OFU1VE0JODS&afp1=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=WTQYJFRG5YI&afp1=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
