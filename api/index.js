cf7c478f93262f9f2742605d0c3727cfd6cae818
JCPrime01 <vanzokiller@gmail.com>
2026-07-03 21:52:09 -0300

    chore: shareCode YEYO6GZ40XH -> XW6PNFUAM5N (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=YEYO6GZ40XH&afp1=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=XW6PNFUAM5N&afp1=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
