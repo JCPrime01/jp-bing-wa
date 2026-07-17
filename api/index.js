282796cbc2c9ae4ea5f78ea58c821c975eeebc10
JCPrime01 <vanzokiller@gmail.com>
2026-07-17 14:33:13 -0300

    chore: shareCode ALI0VV4G5VR -> HOAWFX2Z3RR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=ALI0VV4G5VR&afp1=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=HOAWFX2Z3RR&afp1=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
