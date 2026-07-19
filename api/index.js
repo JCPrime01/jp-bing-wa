f9cab8a1cc0339fc4461ae7fb7ec5e6d49e1c7e9
JCPrime01 <vanzokiller@gmail.com>
2026-07-19 10:59:36 -0300

    chore: shareCode H4EML88WU12 -> K4ZDOIZ3LJ2 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=H4EML88WU12&afp1=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=K4ZDOIZ3LJ2&afp1=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
