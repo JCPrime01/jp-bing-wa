aa22114bdc9b50aacb67568e2c59475c4f8deceb
JCPrime01 <vanzokiller@gmail.com>
2026-07-22 13:00:12 -0300

    chore: shareCode LANJ55EN3EN -> 9SZAGC9EHDS (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=LANJ55EN3EN&afp1=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=9SZAGC9EHDS&afp1=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
