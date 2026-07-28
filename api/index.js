cec53eab1ce354fbc25c5e7065a101167b8c115b
JCPrime01 <vanzokiller@gmail.com>
2026-07-28 13:42:03 -0300

    chore: shareCode RWWGV7E0NUD -> SZ68JETH2Q3 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=RWWGV7E0NUD&afp1=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=SZ68JETH2Q3&afp1=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
