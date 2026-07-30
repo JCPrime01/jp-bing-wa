175c0e0661763e496d8c493fcd83aaf3e40a0c6f
JCPrime01 <vanzokiller@gmail.com>
2026-07-30 12:49:28 -0300

    chore: shareCode 23MX7JT9NEH -> 2WPVHIYNQ2G (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=23MX7JT9NEH&afp1=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=2WPVHIYNQ2G&afp1=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
