36efe79538b4d83b8a5725c3c2ba2776560d98d9
JCPrime01 <vanzokiller@gmail.com>
2026-08-01 12:42:38 -0300

    chore: shareCode 2WPVHIYNQ2G -> OHW7BQAELRZ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=2WPVHIYNQ2G&afp1=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=OHW7BQAELRZ&afp1=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
